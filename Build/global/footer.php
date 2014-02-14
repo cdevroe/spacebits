595503e088024092c7370226f29acc317d887a3d
Colin Devroe <colin@cdevroe.com>
2014-02-14 18:46:54 -0500

    whatever

---
+++ Build/global/footer.php
@@ -0,0 +1,7 @@
+<div class="page-footer">
+	<div class="container clearfix">
+		<div class="grid-3 no-bottom-margin center-text clearfix">
+			<p>© Spacebits. Powered by Barley</p>
+		</div>
+	</div>
+</div>
