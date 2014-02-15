9a26789156711631aecb89ccd71e4bbbfb36d276
Colin Devroe <colin@cdevroe.com>
2014-02-14 22:29:38 -0500

    Several adjustments including homepage adjustments, mastheads are optional.

--- Build/global/footer.php
+++
@@ -1,7 +0,0 @@
-<div class="page-footer">
-	<div class="container clearfix">
-		<div class="grid-3 no-bottom-margin center-text clearfix">
-			<p>© Spacebits. Powered by Barley</p>
-		</div>
-	</div>
-</div>
